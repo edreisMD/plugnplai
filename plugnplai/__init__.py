f3bc77491aaefa851f011691279c5983f6f15911
Eduardo Reis <edu.pontes@gmail.com>
2023-04-17 21:04:03 -0700

    add wraper

--- plugnplai/__init__.py
+++ plugnplai/__init__.py
@@ -1,4 +1,5 @@
-from call_api import CallApi
-from load_plugin import PluginJson, Plugin, InstallPlugins
+from plugnplai.call_api import CallApi
+from plugnplai.load_plugin import PluginJson, Plugin, InstallPlugins
+from plugnplai.add_plugins import AddPlugins
 
-__all__ = ["CallApi", "PluginJson", "Plugin", "InstallPlugins"]
+__all__ = ["CallApi", "PluginJson", "Plugin", "InstallPlugins", "AddPlugins"]
---
+++ plugnplai/add_plugins.py
@@ -0,0 +1,28 @@
+import json
+import re
+import requests
+from typing import Tuple, Callable, Any
+from call_api import CallApi
+
+
+class AddPlugins:
+    def __init__(self, active_plugins):
+        self.active_plugins = active_plugins
+
+    def __call__(self, func: Callable[..., Any]) -> Callable[..., Any]:
+        def wrapper(*args, **kwargs) -> Any:
+            # Call the original function (GPT-4 API call)
+            llm_response = func(*args, **kwargs)
+
+            # Check for the specific pattern (**) in the response
+            if "**" not in llm_response:
+                return llm_response
+
+            # Use the CallApi class to make an API call based on the response
+            call_api = CallApi(llm_response, self.active_plugins)
+            response, message_to_user = call_api.process()
+
+            # Call the original function again with the result of the API call
+            return func(message_to_user, **kwargs)
+
+        return wrapper
