448a0f10964a6240f59f6f2395fd71409520f115
Eduardo Reis <edu.pontes@gmail.com>
2023-05-13 15:45:47 -0700

    Adhering to Toolformer pattern

--- plugnplai/prompt_templates.py
+++ plugnplai/prompt_templates.py
@@ -10,29 +10,21 @@ Knowledge Cutoff: 2021-09
 Current date: DATE_TODAY
 Below is a list of available APIs that you can utilize to fulfill user requests. 
 When using an API, please follow the specified format to make the API call. 
-If possible, avoid asking follow-up questions and aim to complete the task with the information provided by the user.
+Don't ask follow-up questions and aim to complete the task with the information provided by the user.
 
 To make an API call, use the following format:
 
-[API]namespace.operationId[/API]
-[PARAMS]{ 
-    "parameter_name": "parameter_value",
-    ...
-}[/PARAMS]
+<API>namespace.operationId({"parameter_name": "parameter_value", ...})</API>
 
 For example, to call an API operation with the operation ID "productsUsingGET" in the "KlarnaProducts" namespace, 
 and provide the required parameters "q" and "size", the format would be as follows:
 
-[API]KlarnaProducts.productsUsingGET[/API]
-[PARAMS]{
-    "q": "t-shirt", 
-    "size": 3
-}[/PARAMS]
+<API>KlarnaProducts.productsUsingGET({"q": "t-shirt", "size": 3})</API>
 
 Please ensure that you use the correct namespace and operation ID, and provide the necessary parameters for each API call. 
 After requesting the API, refrain from writing anything else and wait for the API response, which will be delivered in a new message.
 
-## Plugins description
+## Plugins description (Required parameters are marked with an asterisk (*):
 
 {{plugins}}
 # USER MESSAGE
