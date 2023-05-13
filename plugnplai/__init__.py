3fa5fe9e39c006987c40bae0e5f8903d2d2a70d1
Eduardo Reis <edu.pontes@gmail.com>
2023-05-12 18:32:47 -0700

    call funtion and example

--- plugnplai/__init__.py
+++ plugnplai/__init__.py
@@ -4,19 +4,23 @@ from plugnplai.utils import (
     get_plugin_manifest,
     get_plugins,
     spec_from_url,
-    get_category_names
+    get_category_names,
+    parse_llm_response
 )
 from plugnplai.embeddings import PluginRetriever
-from plugnplai.plugins import PluginObject, Plugins
+from plugnplai.plugins import PluginObject, Plugins, build_request_body, count_tokens
 
 __all__ = [
     "PluginObject",
     "Plugins",
+    "PluginRetriever",
     "get_plugins",
     "get_plugin_manifest",
     "get_openapi_url",
     "get_openapi_spec",
     "get_category_names",
     "spec_from_url",
-    "PluginRetriever",
+    "parse_llm_response",
+    "build_request_body",
+    "count_tokens"
 ]
